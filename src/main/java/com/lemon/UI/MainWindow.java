272708b711c99bc85018c05a9b5c57a31f2dd45c
poko900517 <poko900517@gmail.com>
2022-05-20 12:43:31 +0800

    2022/05/20-2

--- src/main/java/com/lemon/UI/MainWindow.java
+++ src/main/java/com/lemon/UI/MainWindow.java
@@ -2,6 +2,9 @@ package com.lemon.UI;
 
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow {
     public static void main(String[] args) {
@@ -12,6 +15,18 @@ public class MainWindow {
         frame.setSize(400,300);
         frame.setLocation(200,200);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JButton button = new JButton("ok");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("hahaha");
+            }
+        });
+        frame.setLayout(new FlowLayout());
+        frame.add(button);
+        frame.add(new JButton("Testing"));
+
+        //Layout
         frame.setVisible(true);
         System.out.println("END?");
     }
