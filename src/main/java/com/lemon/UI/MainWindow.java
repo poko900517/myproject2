b3b9c159cde0ac2b781fa6491a4f9b1710700793
poko900517 <poko900517@gmail.com>
2022-05-20 11:28:38 +0800

    2022/05/20-1

---
+++ src/main/java/com/lemon/UI/MainWindow.java
@@ -0,0 +1,18 @@
+package com.lemon.UI;
+
+
+import javax.swing.*;
+
+public class MainWindow {
+    public static void main(String[] args) {
+        //Graphical User Interface(GUI)
+        //swing, JavaFX
+        //extension
+        JFrame frame = new JFrame();
+        frame.setSize(400,300);
+        frame.setLocation(200,200);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+        System.out.println("END?");
+    }
+}
